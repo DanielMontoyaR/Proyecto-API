eac1563c1b3fda743f309795b73e569a95d14e38
DanielMontoyaR <danimo.rivera@gmail.com>
2023-05-06 15:01:43 -0600

    Modificación en el atributo status de la entidad Shop

--- GymTEC-API/Entidades/Shop.cs
+++ GymTEC-API/Entidades/Shop.cs
@@ -3,7 +3,7 @@
     public class Shop
     {
         public string Branch_Name { get; set; } = string.Empty; //FK
-        public string Status { get; set; }
+        public string Status { get; set; } = string.Empty;
 
         //public Product Shop_Product { get; set; } = new Product();
     }
